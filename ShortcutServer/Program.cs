36dd535605afba3632fa3d5d15c14994695b666f
mgcllee <mgcllee@gmail.com>
2024-11-03 16:16:56 +0900

    [Server][upgrade] accept multiple user

--- ShortcutServer/Program.cs
+++ ShortcutServer/Program.cs
@@ -7,40 +7,46 @@ namespace Example
 {
     class Program
     {
-        // 실행 함수
-        static void Main(string[] args)
+        static void client_recver(int client_ticket, Socket client)
         {
-            // Socket EndPoint 설정(서버의 경우는 Any로 설정하고 포트 번호만 설정한다.)
-            var ipep = new IPEndPoint(IPAddress.Any, 7777);
-            // 소켓 인스턴스 생성
-            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            var ip = client.RemoteEndPoint as IPEndPoint;
+            Console.WriteLine($"Client {client_ticket} : (From: {ip.Address.ToString()}:{ip.Port}, Connection time: {DateTime.Now})");
+
+            var sb = new StringBuilder();
+            var binary = new Byte[1024];
+
+            while (true)
             {
-                // 서버 소켓에 EndPoint 설정
-                server.Bind(ipep);
-                // 클라이언트 소켓 대기 버퍼
-                server.Listen(20);
-                // 콘솔 출력
-                Console.WriteLine($"Server Start... Listen port {ipep.Port}...");
-                // 클라이언트로부터 접속 대기
-                using (var client = server.Accept())
+                string data = Console.ReadLine();
+                if (data == null) break;
+                if (int.Parse(data.Substring(0, 2)) == client_ticket)
                 {
-                    var ip = client.RemoteEndPoint as IPEndPoint;
-                    Console.WriteLine($"Client : (From: {ip.Address.ToString()}:{ip.Port}, Connection time: {DateTime.Now})");
-                    
-                    var sb = new StringBuilder();
-                    // 통신 바이너리 버퍼
-                    var binary = new Byte[1024];
-                    // 무한 루프
-                    while (true)
-                    {
-                        string data = Console.ReadLine();
-                        if (data == null) break;
-                        var sendMsg = Encoding.ASCII.GetBytes(data);
-                        client.Send(sendMsg);
-                    }
+                    data = data.Substring(2);
+                    var sendMsg = Encoding.ASCII.GetBytes(data);
+                    client.Send(sendMsg);
                 }
             }
-            // 아무 키나 누르면 종료
+        }
+
+        static void Main(string[] args)
+        {
+            var ipep = new IPEndPoint(IPAddress.Any, 7777);
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.Bind(ipep);
+                server.Listen(20);
+                Console.WriteLine($"Server Start... Listen port {ipep.Port}...");
+
+                List<Thread> clients = new List<Thread>();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    clients.Add(new Thread(() => client_recver(i, server.Accept())));
+                    clients[i].Start();
+                }
+
+                while(true);
+            }
             Console.WriteLine("Press Any key...");
         }
     }
